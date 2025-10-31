8f42082f9e8452b2ec84fecbb5cafbaf278cebd5
Ygor Farias de melo <114826151+Yguin77@users.noreply.github.com>
2025-10-31 20:06:51 -0300

    Delete Codigos directory

--- Codigos/cod7.py
+++
@@ -1,9 +0,0 @@
-import sys
-import array
-
-cities = ["Rio de Janeiro","Brasília","Goiânia"]
-
-sys.stdout.write( str(cities) )
-sys.stdout.write( str(cities[0]) )
-sys.stdout.write( str(cities[1]) )
-sys.stdout.write( str(cities[2]) )
--- Codigos/cod8.py
+++
@@ -1,9 +0,0 @@
-import sys
-import array
-
-cities = ["Rio de Janeiro",897,"Goiânia"]
-
-sys.stdout.write( str(cities) )
-sys.stdout.write( str(cities[0]) )
-sys.stdout.write( str(cities[1]) )
-sys.stdout.write( str(cities[2]) )
